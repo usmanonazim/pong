16f9d03b953c1ca213b11f93aac5d926a6c1eedb
UsmanOmar <omarusman830@gmail.com>
2019-02-26 13:55:49 +0000

    Update ball.js

--- ball.js
+++ ball.js
@@ -1,6 +1,8 @@
 var xspeed = 0;
 var yspeed = 0;
 
+var framecount = 0;
+
 function Ball(x, y) {
   this.x = x;
   this.y = y;
@@ -12,6 +14,7 @@ function Ball(x, y) {
     this.x += this.xspeed;
     this.y += this.yspeed;
 
+    framecount++;
     var v = createVector(this.x, this.y);
     this.history.push(v);
 
@@ -30,8 +33,14 @@ function Ball(x, y) {
 
       fill(col, i * 5);
 
-      if (i % 2 == 0) {
-        ellipse(pos.x, pos.y, i * 0.8);
+      if (framecount % 10 == 0) {
+        if (i % 3 == 0) {
+          ellipse(pos.x, pos.y, i * 0.8);
+        }
+      } else {
+        if (i % 2 == 0) {
+          ellipse(pos.x, pos.y, i * 0.8);
+        }
       }
     }
   };
